51b664f8d196f48adc99f6a645de05c6ed1ce150
Thanawat Kanphuton <framegg908@gmail.com>
2025-02-01 12:33:38 +0700

    "ยกเครื่อง"

--- frontend/src/lib/utils.tsx
+++
@@ -1,8 +0,0 @@
-// lib/utils.ts
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-// ฟังก์ชันสำหรับรวม class names จาก Tailwind CSS
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
