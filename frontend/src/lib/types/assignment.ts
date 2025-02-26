ab5f27dcdb83a13fcc0c070e1a28dff8ba8fe0a2
Thanawat Kanphuton <framegg908@gmail.com>
2025-02-26 23:05:13 +0700

    .

--- frontend/src/lib/types/assignment.ts
+++
@@ -1,8 +0,0 @@
-// frontend/lib/types/assignment.ts
-export interface Assignment {
-    id: string;
-    title: string;
-    description: string;
-    created_at: string;
-    updated_at: string;
-  }
